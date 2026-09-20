06ee301ccf044e8b984caa1a9dc7b39efefa5b12
Mariana Lebrão <lebrao.mariana@gmail.com>
2026-09-20 00:41:06 -0300

    Add Firebase App Check to production site

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "firebase/app-check";
 import { getFirestore } from "firebase/firestore";
 
 const app = initializeApp({
@@ -9,5 +10,12 @@ const app = initializeApp({
   messagingSenderId: "1026691533718",
   appId: "1:1026691533718:web:6490f86be7ecbb27dfebb7",
 });
+
+if (import.meta.env.PROD && !["localhost", "127.0.0.1"].includes(window.location.hostname)) {
+  initializeAppCheck(app, {
+    provider: new ReCaptchaEnterpriseProvider("6LcezsQtAAAAAFKsxecHbge1cdQBMCtJGpzazfT3"),
+    isTokenAutoRefreshEnabled: true,
+  });
+}
 
 export const db = getFirestore(app);
